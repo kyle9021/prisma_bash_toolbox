c1c2f3db9442c85dc746829a5adb0e2c1fa3baa2
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2021-11-05 14:27:47 -0400

    Update update_existing_user.sh

--- update_existing_user.sh
+++ update_existing_user.sh
@@ -17,14 +17,16 @@ KEYS_ALLOWED="<true_or_false>"
 # Adjust timezone as you see fit. 
 TIME_ZONE="America/Los_Angeles"
 
-AUTH_PAYLOAD_PRE='{"username": "%s", "password": "%s"}'
-AUTH_PAYLOAD=$(printf "$AUTH_PAYLOAD_PRE" "$PC_ACCESSKEY" "$PC_SECRETKEY")
+AUTH_PAYLOAD=$(cat <<EOF
+{"username": "$PC_ACCESSKEY", "password": "$PC_SECRETKEY"}
+EOF
+)
 
 PC_JWT=$(curl --request POST \
               --url "$PC_APIURL/login" \
               --header 'Accept: application/json; charset=UTF-8' \
               --header 'Content-Type: application/json; charset=UTF-8' \
-              --data "${AUTH_PAYLOAD}" | jq -r '.token')
+              --data "$AUTH_PAYLOAD" | jq -r '.token')
 
 ROLE_JSON=$(curl --request GET \
                        --url "$PC_APIURL/v2/user/{$USER_EMAIL}" \
@@ -38,23 +40,25 @@ declare -a ROLE_ID_ARRAY=$(printf %s $ROLE_JSON | jq -r '.roleIds[]' )
 # To add more roles simply add lines under the roleId section with more "%s"
 # If you do make sure to add the indexes to the variable ${ROLE_ID_ARRAY[<INDEX_NUMBER_HERE>]} in the $USER_PAYLOAD_VAR
 
-USER_PAYLOAD_PRE='{
-  "email": "%s",
-  "firstName": "%s",
-  "lastName": "%s",
-  "enabled": "%s",
-  "accessKeysAllowed": "%s",
-  "defaultRoleId": "%s",
+USER_PAYLOAD=$(cat <<EOF
+{
+  "email": "$USER_EMAIL",
+  "firstName": "$USER_FIRSTNAME",
+  "lastName": "$USER_LASTNAME",
+  "enabled": "$ENABLED",
+  "accessKeysAllowed": "$KEYS_ALLOWED",
+  "defaultRoleId": "$DEFAULT_ROLE_ID",
   "roleIds": [
-    "%s"
+    "${ROLE_ID_ARRAY[0]}"
   ],
-  "timeZone": "%s"
-}'
+  "timeZone": "$TIME_ZONE"
+}
+EOF
+)
 
-USER_PAYLOAD=$(printf "$USER_PAYLOAD_PRE" "$USER_EMAIL" "$USER_FIRSTNAME" "$USER_LASTNAME" "$ENABLED" "$KEYS_ALLOWED" "$DEFAULT_ROLE_ID" "${ROLE_ID_ARRAY[0]}" "$TIME_ZONE" )
 
 curl --request PUT \
-  --url https://api3.prismacloud.io/v2/user/{$CTF_USER} \
+  --url "$PC_APIURL/v2/user/{$CTF_USER}" \
   --header 'content-type: application/json' \
   --header "x-redlock-auth: $PC_JWT" \
   --data "$USER_PAYLOAD"
